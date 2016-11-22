7a2d80e95aaa97e5cc8586a91276346c4bf47880
Corw <pzamyatin@gmail.com>
2016-11-22 22:30:26 +1300

    JSON structure added

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ myApp.run(function() {
 })
 
 // [] - dependencies, include $scope and func to prevent any conflict
-myApp.controller('MyFirstController', ['$scope', function($scope) {
+myApp.controller('MyFirstController', ['$scope', '$http', function($scope, $http) {
   // $scope.message = 'Hey, whatsapp, bro?';
 
   // remove element from the list
@@ -35,11 +35,11 @@ myApp.controller('MyFirstController', ['$scope', function($scope) {
   // add element to the list using submit
   $scope.addPerson = function() {
     $scope.list.push({
-        name: $scope.newPerson.name,
-        belt: $scope.newPerson.belt,
-        rate: parseInt($scope.newPerson.rate),
-        available: true
-      })
+      name: $scope.newPerson.name,
+      belt: $scope.newPerson.belt,
+      rate: parseInt($scope.newPerson.rate),
+      available: true
+    })
 
     // clean the form
     $scope.newPerson.name = '';
@@ -47,34 +47,12 @@ myApp.controller('MyFirstController', ['$scope', function($scope) {
     $scope.newPerson.rate = '';
   }
 
-  $scope.list = [
-    {
-      name: 'Corw',
-      belt: 'blue',
-      rate: 10,
-      available: true,
-      thumb: 'content/img/crw.png'
-    },
-    {
-      name: 'Kate',
-      belt: 'black',
-      rate: 30,
-      available: true,
-      thumb: 'content/img/kate.png'
-    },
-    {
-      name: 'Ackerman',
-      belt: 'orange',
-      rate: 90,
-      available: true,
-      thumb: 'content/img/acker.png'
-    },
-    {
-      name: 'Dmitry',
-      belt: 'green',
-      rate: 60,
-      available: false,
-      thumb: 'content/img/dima.png'
-    }
-  ];
+  $http .get('data/list.json')
+        .success(function(data) {
+          $scope.list = data;
+        })
+
+  // convert array of objects into the json file structure
+  console.log(angular.toJson($scope.list));
+
 }])
